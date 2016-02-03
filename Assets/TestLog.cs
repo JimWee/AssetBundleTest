a32c781926a34d68d7d8585c596acc79c057ac24
JianhuiWei <18817551783@163.com>
2016-02-03 18:17:28 +0800

    升级至5.3.2|删除导入vs插件

---
+++ Assets/TestLog.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestLog : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
