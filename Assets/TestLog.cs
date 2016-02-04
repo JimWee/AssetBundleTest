f0877fe560c64d2b0211238b7dd9499d4807d7bc
JianhuiWei <18817551783@163.com>
2016-02-04 11:06:08 +0800

    测试加载脚本

--- Assets/TestLog.cs
+++ Assets/TestLog.cs
@@ -6,6 +6,7 @@ public class TestLog : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        go.transform.position = new Vector3(transform.position.x + 2, transform.position.y, transform.position.z);
 	}
 	
 	// Update is called once per frame
